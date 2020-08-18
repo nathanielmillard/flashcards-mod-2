2864686a602ea7051fcbda0f70fdcba0f2553b8f
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-17 19:43:45 -0600

    Add tests for Turn's two arguments

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -12,4 +12,16 @@ describe('Turn', function(){
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
+
+  it.skip('should store a guess', function() {
+    const turn = new Turn('pug');
+    expect(turn.guess).to.equal('pug');
+  });
+
+  it.skip('should store a Card', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.card).to.deep.equal(card);
+  });
+
 })
