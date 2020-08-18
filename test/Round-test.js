d86059e1709014f4928d6bcaef959f444a0bc0d5
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-18 16:48:44 -0600

    Add test for tracking current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -151,4 +151,29 @@ describe('Round', function() {
     round.takeTurn('spleen');
     expect(round.incorrectGuesses).to.equal([14]);
   })
+
+  it.skip('should keep track of current card', function(){
+    const card1 = new Card(
+      1,
+      'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter');
+    const card2 = new Card(
+      14,
+      'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder');
+    const card3 = new Card(
+      12,
+      'What is Travis\'s favorite stress reliever?',
+      ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
+      'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    round.takeTurn('sea otter');
+    round.takeTurn('spleen');
+    expect(round.returnCurrentCard()).to.deep.equal(card3);
+  })
+
+
 })
