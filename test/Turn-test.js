7bff02c21e660fd14c3120c4f20a4b32e908c258
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-17 19:35:31 -0600

    Create first test of 'should be a function'

---
+++ test/Turn-test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function(){
+  it('should be a function', function(){
+    expect(Turn).to.be.a('function');
+  });
+})
