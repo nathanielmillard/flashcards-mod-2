ca1c6c746909b46d0ec433b5deabb80c0909eebd
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-17 19:56:12 -0600

    Create Turn constructor to pass first two tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,11 +4,11 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn', function(){
-  it.skip('should be a function', function(){
+  it('should be a function', function(){
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it('should be an instance of Turn', function() {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
