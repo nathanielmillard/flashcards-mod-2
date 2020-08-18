904d3b4d005699f16c6920ef49490073a2f0b0b0
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-18 16:25:15 -0600

    Add tests for key value pairs

--- test/Round-test.js
+++ test/Round-test.js
@@ -10,8 +10,22 @@ describe('Round', function() {
     expect(Round).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function() {
+  it.skip('should be an instance of Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
+  });
+
+  it('should have a default number of turns', function() {
+    const round = new Round;
+    expect(round.turns).to.equal(0);
+  });
+
+  it('should store a deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
   });
 })
