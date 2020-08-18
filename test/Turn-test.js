601e9aa537c16a5747906220f99594f3095903ea
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-17 19:37:26 -0600

    Create first test of 'should be an instance of Turn'

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,7 +4,12 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn', function(){
-  it('should be a function', function(){
+  it.skip('should be a function', function(){
     expect(Turn).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
   });
 })
