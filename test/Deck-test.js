6748cdfadca0f2b69b5fe979207f76df94eef14e
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-18 11:42:45 -0600

    Creaet tests for class functionality

---
+++ test/Deck-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+  it.skip('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Turn', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+})
