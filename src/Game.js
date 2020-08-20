462f4b84eba36687d42464fa3b44588a7dc45e32
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-20 14:04:12 -0600

    Added in lines to start game

--- src/Game.js
+++ src/Game.js
@@ -2,12 +2,18 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
-class Game {
-  constructor() {
+//I might want to use a for each loop to take each object in this array
+//and intantiate it as a new instance of a 'Card' each time and put those in an array
+//then take this new array and turn it into a Deck
+//pass that deck into a Round
 
+
+class Game {
+  constructor(cardsArray) {
+    this.cards = cardsArray
   }
-  start(prototypeQuestions){
-    deck = new Deck(prototypeQuestions);
+  start(cards){
+    deck = new Deck(cards);
     round = new Round(deck);
     this.printMessage();
     this.printQuestion();
