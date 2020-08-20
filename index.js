36a852599ce38f57a672306dabdcdd389e3dadb6
Nathaniel Millard <nathanielmillard@gmail.com>
2020-08-20 15:21:53 -0600

    Debugg lint errors

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+const Game = require('./src/Game.js');
 const http = require('http');
 let app = http.createServer();
-//require Game.js
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
 
-const game = new Game ()
+const game = new Game()
 game.start();
-//to instantiate a new Game object
--- src/Game.js
+++ src/Game.js
@@ -2,41 +2,34 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 const Card = require('../src/Card');
-//I might want to use a for each loop to take each object in this array
-//and intantiate it as a new instance of a 'Card' each time and put those in an array
-//then take this new array and turn it into a Deck
-//pass that deck into a Round
-
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
 
 class Game {
   constructor() {
-    this.currentRound = ''
-    this.cards = []
+    this.currentRound = '';
+    this.cards = [];
   }
-  start(){
-    makeCards();
-    deck = new Deck(cards);
-    round = new Round(deck);
-    this.printMessage();
-    this.printQuestion();
+  start() {
+    this.makeCards();
+    const deck = new Deck(this.cards);
+    const round = new Round(deck);
+    this.printMessage(deck);
+    this.printQuestion(round);
   }
-
-  makeCards(){
+  makeCards() {
     let newArray = prototypeQuestions.map(card => {
       return new Card(card.id, card.question, card.answers, card.correctAnswer)
     })
     this.cards = newArray
   }
-
-  printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+  printMessage(deck) {
+  console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
-
   printQuestion(round) {
       util.main(round);
   }
-
 }
 
 module.exports = Game;
